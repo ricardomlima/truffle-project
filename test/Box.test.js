1a3f4669e2f57bbb9e04f1e8f535857114c73afd
Ricardo M. Lima <ricardolima89@gmail.com>
2022-05-30 20:43:13 -0300

    testing contract events with test helpers

--- test/Box.test.js
+++ test/Box.test.js
@@ -2,24 +2,38 @@
 // Load dependencies
 const { expect } = require('chai');
 
+// Import utilities from Test Helpers
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+
 // Load compiled artifacts
 const Box = artifacts.require('Box');
 
 // Start test block
-contract('Box', function () {
+contract('Box', function ([owner, other]) {
+
+  // Use large integers ('big numbers')
+  const value = new BN('42');
+
   beforeEach(async function () {
     // Deploy a new Box contract for each test
-    this.box = await Box.new();
+    this.box = await Box.new({ from: owner });
   });
 
   // Test case
   it('retrieve returns a value previously stored', async function () {
     // Store a value
-    await this.box.store(42);
+    await this.box.store(value,{ from: owner });
 
     // Test if the returned value is the same one
     // Note that we need to use strings to compare the 256 bit integers
     //   console.log(await this.box.retrieve().toString())
-    expect((await this.box.retrieve()).toString()).to.equal('42');
+    expect((await this.box.retrieve()).toString()).to.be.bignumber.equal(value);
   });
+
+  it('store emits an even', async function () {
+    const receipt = await this.box.store(value, { from: owner });
+
+    expectEvent(receipt, 'ValueChanged', { value: value });
+  });
+
 });
